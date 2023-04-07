d1422e3b058b4f90d52dc41b1a1f37a135a43088
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-04-07 17:40:46 +0600

    Left Shift Operator

---
+++ Left_shift.cpp
@@ -0,0 +1,11 @@
+// For left shift, The initial number will be increase
+// n<<i = n*pow(2,i);
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=0;i<11;i++)
+    {
+        cout<< (1<<i)<<endl;
+    }
+}
