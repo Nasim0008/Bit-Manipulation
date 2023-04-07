b2e6262020b9cb2dde195d83feb4758edd58911b
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-04-07 17:44:33 +0600

    Right shift operator

---
+++ Right_shift.cpp
@@ -0,0 +1,12 @@
+// For right shift, The initial number will be decreased
+// n>>i == n/(pow(2,i))
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=0;i<11;i++)
+    {
+        cout<<(1024>>i)<< endl;
+    }
+}
