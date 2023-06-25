1711e91aa7977c508e599e21b0cc5ece0517b486
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-06-25 16:58:59 +0600

    reset all the bits as 0

---
+++ Bitset/reset.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    bitset<8>bset(100);
+    cout<<bset<<endl;
+
+    bset.reset();// initialize all the value by 0
+    cout<<bset<<endl;
+}
