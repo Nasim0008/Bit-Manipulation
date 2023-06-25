008c745a918e8d139d2318566ccd214f286dde80
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-06-25 16:57:07 +0600

    initialize all the bit as one

---
+++ Bitset/initialize_as_set.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    bitset<8>bset(100);
+    cout<<bset<<endl;
+
+    bset.set();// initialize all the value by 1
+    cout<<bset<<endl;
+}
